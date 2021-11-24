5d90e36b0e36fe977c104e9df09844fd00f64a69
Matheus Guanaz <matheusguanaz@hotmail.com>
2021-11-24 16:00:17 -0300

    ignorando arquivo

--- insertIntoDB.py
+++ insertIntoDB.py
@@ -1,6 +1,6 @@
 from utils import con
 
-file = open('palavras.txt','r')
+file = open('palavras.txt','r',encoding='utf-8')
 
 
 alfabeto = ['A', 'B', 'C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a', 'b', 'c', 'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
@@ -13,3 +13,5 @@ for palavra in file.readlines():
     cursor.execute("INSERT INTO {0} VALUES('{1}')".format(alfabeto[i], palavra))
     con.commit()
     print(i)
+con.close()
+file.close()

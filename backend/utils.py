e0a046a8f26889830f1c214b7239d9ada40b54df
Matheus Guanaz <matheusguanaz@hotmail.com>
2021-11-24 15:56:03 -0300

    iniciando e fechando conexao dentro das funções

--- backend/utils.py
+++ backend/utils.py
@@ -4,15 +4,19 @@ con = psycopg2.connect(os.environ['DB_CONNECTION'])
 
 
 def select(query):
+    con = psycopg2.connect(os.environ['DB_CONNECTION'])
     cursor = con.cursor()
     cursor.execute(query)
     result = cursor.fetchall()
+    con.close()
     return result
 
 def executeQuery(query):
+    con = psycopg2.connect(os.environ['DB_CONNECTION'])
     cursor = con.cursor()
     cursor.execute(query)
     con.commit()
+    con.close()
 
 
 
